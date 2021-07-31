21e2d70063ef9150cb8a3a360b862fd89c59b108
ThaaoBlues <thaaoblues81@gmail.com>
2021-07-31 13:41:58 +0200

    better handling of linux term emulators

--- pmanager/terminal.py
+++ pmanager/terminal.py
@@ -36,30 +36,43 @@ def initialize(namespace):
     pinfo(f"opening project folder : \n {dirpath}")
     
     
+    DEBIAN_BASED_DISTRO = ["debian","ubuntu","linuxmint","kali","raspbian","deepin","antix","parrot","devuan","av"]
 
 
     if system() == "Linux":
-        
-        term_emulator = sep("-| ",getenv('TERM'))[0].strip("\n")
 
-        pinfo(f"detected default terminal emulator : {term_emulator}")
+        if id() in DEBIAN_BASED_DISTRO:
 
-        try:
-            Process(target=run_cmd,args=(f"{term_emulator} --workdir {dirpath}",)).start()
-
-            pwarn(f"running : {term_emulator} --workdir {dirpath}")
-        except CalledProcessError:
             try:
-                Process(target=run_cmd,args=(f"{term_emulator} --working-directory {dirpath}",)).start()
+            
+                Process(run_cmd(f"x-terminal-emulator --working-directory \"{dirpath}\"")).start()
+            except:
+                perror("The command returned a non-zero status, pmanager is struggling to handle linux terminal emulators so it may be normal. Sorry bro x')")
+            
+        
+        else:
+            pwarn("non debiand-based distro, I will die before finishing to handle the amount of terminal emulators here.\n Trying some not really working things...")
+        
+            term_emulator = sep("-| ",getenv('TERM'))[0].strip("\n")
 
-                pwarn(f"running : {term_emulator} --working-directory {dirpath}")
+            pinfo(f"maybe detected default terminal emulator : {term_emulator}")
+
+            try:
+                Process(target=run_cmd,args=(f"{term_emulator} --workdir {dirpath}",)).start()
+
+                pwarn(f"running : {term_emulator} --workdir {dirpath}")
             except CalledProcessError:
                 try:
-                    Process(target=run_cmd,args=(f"{term_emulator} --w {dirpath}",)).start()
+                    Process(target=run_cmd,args=(f"{term_emulator} --working-directory {dirpath}",)).start()
 
-                    pwarn(f"running : {term_emulator} --w {dirpath}")
+                    pwarn(f"running : {term_emulator} --working-directory {dirpath}")
                 except CalledProcessError:
-                    perror("Sorry, pmanager is not yet able to identify and start an instance of your terminal")
+                    try:
+                        Process(target=run_cmd,args=(f"{term_emulator} --w {dirpath}",)).start()
+
+                        pwarn(f"running : {term_emulator} --w {dirpath}")
+                    except CalledProcessError:
+                        perror("Sorry, pmanager is not yet able to identify and start an instance of your terminal")
 
     
     
